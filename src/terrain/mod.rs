e60bed708fc443a5a018e74b688b06355b4942e6
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2025-07-17 17:22:22 +1000

    add pos impls

--- src/terrain/mod.rs
+++ src/terrain/mod.rs
@@ -1,5 +1,40 @@
+use core::ops::Range;
+
 
 
 
 pub mod world;
 pub mod chunk;
+
+
+pub const REGION_SIZE: f64 = 64.0;
+pub const REGION_CHUNKS: Range<usize> = 5..10;
+
+
+
+pub struct RegionPos {
+    pub x: i32,
+    pub y: i32
+}
+
+
+
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Pos {
+    pub x: f64,
+    pub y: f64
+}
+
+impl From<RegionPos> for Pos {
+    fn from(region_pos: RegionPos) -> Self {
+        return Pos::new(region_pos.x as f64 * REGION_SIZE, region_pos.y as f64 * REGION_SIZE);
+    }
+}
+
+impl Pos {
+    pub fn new(x: f64, y: f64) -> Self {
+        return Self { x: x, y: y };
+    }
+}
