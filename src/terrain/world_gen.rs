df29f8d63a28fd377344d0a0a35214703f93c9e0
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2025-07-17 18:03:16 +1000

    a

--- src/terrain/world_gen.rs
+++ src/terrain/world_gen.rs
@@ -1,17 +1,38 @@
-use spade::{DelaunayTriangulation, Point2};
+use spade::{DelaunayTriangulation, Point2, Triangulation};
 
-use crate::terrain::{chunk::Chunk, Pos};
-
-
+use crate::terrain::{chunk::Chunk, world_save::WorldSave, Pos};
 
 
 
 
+
+/// Creates the mesh from a `WorldSave`
 pub struct WorldGen<'world> {
     pub centroids: Vec<Pos>,
     pub triangulation: DelaunayTriangulation<Point2<f64>>,
     pub chunks: Vec<Chunk<'world>>,
 }
 
+
+impl WorldGen<'_> {
+    pub fn init(world_save: WorldSave, player_pos: Pos, radius: f64) -> Self {
+        let mut centroids: Vec<Pos> = Vec::new();
+
+        for region in world_save.regions.values() {
+            for pos in region {
+                let taxicab_distance = (pos.x - player_pos.x).abs() + (pos.y - player_pos.y).abs();
+                if taxicab_distance <= radius {
+                    centroids.push(*pos);
+                };
+            }
+        }
+
+        return Self {
+            centroids: centroids,
+            triangulation: DelaunayTriangulation::new(),
+            chunks: Vec::new()
+        };
+    }
+}
 
 
